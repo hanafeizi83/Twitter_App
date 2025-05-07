ec4b20af19033072860cd8e28e5ae560a62c6221
hana feizi <hanafaizi83@gmail.com>
2025-05-07 10:02:55 +0330

    add profile to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,15 @@ import SignupPage from './feature/auth/signup/SignupPage'
 import Home from './pages/Home'
 import AppLayout from './ui/AppLayout'
 import Notification from './pages/Notification'
-
+import Profile from './pages/Profile'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<AppLayout />}>
           <Route path='/' element={<Home />} />
-           <Route path='/notification' element={<Notification />} />
-         {/* <Route path='/profile/:username' element={<Profile />} /> */}
+          <Route path='/notification' element={<Notification />} />
+          <Route path='/profile/:username' element={<Profile />} />
         </Route>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
