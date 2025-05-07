c0197faaa29a25b31159143aaef94503c8698233
hana feizi <hanafaizi83@gmail.com>
2025-05-07 09:30:35 +0330

    make rightPanel ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import LoginPage from './feature/auth/login/LoginPage'
 import SignupPage from './feature/auth/signup/SignupPage'
+import RightPanel from './ui/RightPanel'
 import Sidebar from './ui/Sidebar'
 
 function App() {
   return (
     <>
    <Sidebar />
+   <RightPanel/>
     </>
   )
 }
---
+++ frontend/src/ui/RightPanel.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { USERS_FOR_RIGHT_PANEL } from './../utils/db/dummy'
+import { Link } from 'react-router-dom';
+function RightPanel() {
+    return (
+        <div className='bg-black w-[22%] rounded-md p-2 top-0 right-0 hidden lg:sticky self-start  lg:flex flex-col'>
+            <h1 className='font-extrabold text-secondary-900 text-center mb-2'>Who to follow</h1>
+            {
+                USERS_FOR_RIGHT_PANEL.map(user => {
+                    return <div key={user._id} className='grid grid-cols-[40px_1fr_65px] grid-rows-1 items-center mb-4'>
+                        <Link to={`profile/${user.username}`}>
+                            <img src={user.profileImg} alt="" className='w-10 h-10' />
+                        </Link>
+
+                        <Link to={`profile/${user.username}`} className='text-center text-secondary-800'>
+                            <h2 className='font-extrabold text-sm'>{user.fullName}</h2>
+                            <p className='text-[12px] text-secondary-500'>@{user.username}</p>
+                        </Link>
+                        <button className='bg-secondary-900 text-secondary-0 font-lg font-bold rounded-full p-1'>Follow</button>
+                    </div>
+                })
+            }
+
+        </div>
+    )
+}
+
+export default RightPanel
