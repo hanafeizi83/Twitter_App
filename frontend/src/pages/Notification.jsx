703fd52e4b6a33d7b308aa102d71c58a3a95c99c
hana feizi <hanafaizi83@gmail.com>
2025-05-09 14:32:45 +0330

    show all notification

--- frontend/src/pages/Notification.jsx
+++ frontend/src/pages/Notification.jsx
@@ -8,7 +8,7 @@ function Notification() {
     const notificationType = 'liked';
     const { notifications } = useNotification();
     console.log(notifications);
-    
+
     return (
         <div className='border-x border-secondary-300 lg:w-[56%] h-screen w-full'>
             {/* Notification Header */}
@@ -19,20 +19,27 @@ function Notification() {
             </div>
 
             <div>
-                <div className='flex p-4 gap-2 border-y border-y-secondary-300'>
-                    {notificationType == 'followed' && <FaUser className='w-7 h-7 text-primary-800' />}
-                    {notificationType == 'liked' && <FaHeart className='w-7 h-7 text-red-500' />}
-                    <div className='flex flex-col items-center'>
-                        <Link to={`/profile/hana`}>
-                            <img src="/avatars/boy2.png" alt="profileImg" className='w-8 h-8' />
-                        </Link>
-                        <Link to={`/profile/hana`} className='flex text-secondary-700 gap-2'>
-                            <h3 className='font-bold'>@hana</h3>
-                            {notificationType == 'followed' && <p className='text-secondary-700'>followed you</p>}
-                            {notificationType == 'liked' && <p className='text-secondary-700'>liked your post</p>}
-                        </Link>
-                    </div>
-                </div>
+                {
+                    notifications && notifications.map((item) => {
+                        console.log(item);
+                        
+                        return <div className='flex p-4 gap-2 border-y border-y-secondary-300'>
+                            {item.type == 'follow' && <FaUser className='w-7 h-7 text-primary-800' />}
+                            {item.type == 'like' && <FaHeart className='w-7 h-7 text-red-500' />}
+                            <div className='flex flex-col items-center'>
+                                <Link to={`/profile/hana`}>
+                                    <img src="/avatars/boy2.png" alt="profileImg" className='w-8 h-8' />
+                                </Link>
+                                <Link to={`/profile/hana`} className='flex text-secondary-700 gap-2'>
+                                    <h3 className='font-bold'>@hana</h3>
+                                    {item.type == 'follow' && <p className='text-secondary-700'>followed you</p>}
+                                    {item.type == 'like' && <p className='text-secondary-700'>liked your post</p>}
+                                </Link>
+                            </div>
+                        </div>
+                    })
+                }
+
             </div>
         </div>
     )
