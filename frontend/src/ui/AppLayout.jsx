798ff104bc672b76e15568cef0ab34933b2a5b13
hana feizi <hanafaizi83@gmail.com>
2025-05-27 21:23:40 +0330

    make some change in responsive

--- frontend/src/ui/AppLayout.jsx
+++ frontend/src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import RightPanel from './RightPanel'
 import { Outlet } from 'react-router-dom'
 function AppLayout() {
   return (
-    <div className='lg:max-w-screen-lg grid pt-5 !w-full grid-cols-[5rem_1fr] sm:grid-cols-[8rem_1fr] md:grid-cols-[13rem_1fr] lg:grid-cols-[13rem_1fr_16rem] container'>
+    <div className='lg:max-w-screen-lg grid pt-5 !w-full grid-cols-[3rem_1fr] md:grid-cols-[13rem_1fr] lg:grid-cols-[13rem_1fr_16rem] container'>
       <Sidebar />
       <Outlet />
       <RightPanel />
