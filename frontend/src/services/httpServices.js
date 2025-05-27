0d2b3b3dbd0f3565e16a0378509d14f746ef8a37
hana feizi <hanafaizi83@gmail.com>
2025-05-27 08:34:57 +0330

    add url deploy

--- frontend/src/services/httpServices.js
+++ frontend/src/services/httpServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://twitter-app-8nzt.onrender.com';
 
 const app = axios.create({
     baseURL: BASE_URL,
